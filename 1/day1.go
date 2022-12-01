28aa6b20f1d8f7dcfafe04cbcdebb2f4fb315d58
Michael Johansson <mikejoh12@gmail.com>
2022-12-01 08:14:14 -0600

    refactor day1

--- 1/day1.go
+++ 1/day1.go
@@ -13,24 +13,23 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
-	var elfCal int
-	var elfCals []int
+	var elfCals = []int{0}
 
 	for scanner.Scan() {
 		t := scanner.Text()
+		var elfCal int
 		if t != "" {
 			cal, _ := strconv.Atoi(t)
-			elfCal += cal
+			elfCals[len(elfCals)-1] += cal
 		} else {
 			elfCals = append(elfCals, elfCal)
-			elfCal = 0
 		}
 	}
 
-	elfCals = append(elfCals, elfCal)
 	sort.Slice(elfCals, func(i, j int) bool {
 		return elfCals[i] > elfCals[j]
 	})
-	
-	fmt.Println(elfCals[0]+elfCals[1]+elfCals[2])
+
+	fmt.Println("part 1:", elfCals[0])
+	fmt.Println("part 2:", elfCals[0]+elfCals[1]+elfCals[2])
 }
