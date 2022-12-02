974d03958970109216b59c682af75ba03b6d44f7
Michael Johansson <mikejoh12@gmail.com>
2022-12-02 09:04:32 -0600

    day 2

---
+++ 2/day2-1.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var score int
+
+	for scanner.Scan() {
+		t := scanner.Text()
+		move := strings.Split(t, " ")
+
+		switch {
+		case move[0] == "A" && move[1] == "Y":
+			score += 2 + 6
+		case move[0] == "A" && move[1] == "Z":
+			score += 3
+		case move[0] == "B" && move[1] == "X":
+			score += 1
+		case move[0] == "B" && move[1] == "Z":
+			score += 3 + 6
+		case move[0] == "C" && move[1] == "X":
+			score += 1 + 6
+		case move[0] == "C" && move[1] == "Y":
+			score += 2
+		case move[0] == "A" && move[1] == "X":
+			score += 1 + 3
+		case move[0] == "B" && move[1] == "Y":
+			score += 2 + 3
+		case move[0] == "C" && move[1] == "Z":
+			score += 3 + 3
+		}
+		
+	}
+	fmt.Println(score)
+}
